4b23ca959b6cc94cb61631ba4e65e1abc309ad12
Nikita <Lojoizz13@mail.ru>
2019-09-22 10:53:10 +0300

    Добавил плавную прокрутку

--- Черновик/js/script.js
+++ Черновик/js/script.js
@@ -1,3 +1,5 @@
-$('a').on("click", function(e) {
+$("body").on('click', '[href*="#"]', function(e){
+	var fixed_offset = 100;
+	$('html,body').stop().animate({ scrollTop: $(this.hash).offset().top - fixed_offset }, 1000);
 	e.preventDefault();
-})
+  });
